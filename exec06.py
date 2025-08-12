8b6c56cf4c187f02331ffb2b2f0c3845641ea23e
Renato Rosa dos Santos Filho <renatto.rfilho@yahoo.com>
2025-08-12 08:53:43 -0300

    Add files via upload
    Exercises List 4 - Looping with :
                                                      while
                                                      for
    Functions:  append
                      SUM
                      MIN 
                      MAX
                     reverse

--- exec06.py
+++ exec06.py
@@ -1,24 +1,30 @@
 """
-6.	Faça um Programa que peça as quatro notas de 10 alunos, calcule e armazene num vetor a média de cada aluno,
+6.	Faça um Programa que peça as quatro notas de 10 alunos, calcule e 
+armazene num vetor a média de cada aluno,
 imprima o número de alunos com média maior ou igual a 7.0.
 """
 
-notas = []
-alunos = 10
-soma = media = 0
-aluno = 0
-nota = 0 
-for i in range(4):
-    print(f' Nota do {i+1}º Aluno')
-    nota = float(input (f" Informe a {i+1}ª Nota: "))
-   
-    for j in range(3):
-     
-     nota = float(input (f" Informe a {j+2}ª Nota: "))
-     notas.append(nota)
+alunos = 3
+
+for aluno in range(1, alunos + 1):
+    notas = []
+    print(f'\n Nota do {aluno}º Aluno: ')
     
-media = sum(notas) / 4
-print(media)
+    
+    for i in range(1, 5):
+        
+        nota = float(input(f" Informe a {i}ª Nota: "))
+        notas.append(nota)
+
+        #notas.append(nota)
+    media = sum(notas) / 4
+    print(f" Notas do ano letivo: {notas}")
+    print(f" Média do {aluno}º Aluno: {media:.2f} ")
+    
+    
+
+
+
     
 
 
